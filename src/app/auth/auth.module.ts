d556d9caf9a04b40b582cdef6ae5040e8d194a65
Fabricio <gromanec@hotmail.com>
2025-01-21 18:01:09 -0500

    Quitando dependencia de CommonModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from "@angular/core";
 import { AuthService } from "./services/auth.service";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { RegisterComponent } from "./components/register/register.component";
-import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from "@angular/forms";
 
 
 @NgModule({
     declarations: [RegisterComponent],
     imports: [
-        CommonModule,
         AuthRoutingModule,
         ReactiveFormsModule
     ],
